eead50cd895f6bff037739678673c409bb28235f
sszuev <sss.zuev@gmail.com>
2022-04-10 22:34:02 +0300

    ont-converter: add Dependencies helper, add tests

---
+++ src/main/kotlin/com/github/sszuev/ontconverter/utils/Dependencies.kt
@@ -0,0 +1,79 @@
+package com.github.sszuev.ontconverter.utils
+
+/**
+ * Finds independent components using depth-first-search.
+ *
+ * @param X - anything
+ * @param [graph] directed graph in form of [Map] of [Collection] of [X]
+ * @return [List] of [Set] of [X], where item-set is a component
+ */
+fun <X> findIndependentComponents(graph: Map<X, Collection<X>>): List<Set<X>> {
+    val res: MutableList<Set<X>> = ArrayList()
+    var root: X? = firstRootOrNull(graph)
+    if (root == null) {
+        res.add(toFlatSet(graph))
+        return res
+    }
+    val map: MutableMap<X, Collection<X>> = graph.toMutableMap()
+    while (map.isNotEmpty()) {
+        if (root == null) {
+            res.add(toFlatSet(map))
+            break
+        }
+        val vertexes: MutableSet<X> = HashSet()
+        dfs(root, graph, HashSet()) { x: X ->
+            vertexes.add(x)
+            map.remove(x)
+        }
+        res.add(vertexes)
+        root = firstRootOrNull(map)
+    }
+    return res
+}
+
+/**
+ * Performs the Depth-First Search on the given graph (recursive implementation).
+ *
+ * @param X - anything
+ * @param [vertex]
+ * @param [graph]
+ * @see [wiki: DFS](https://en.wikipedia.org/wiki/Depth-first_search)
+ */
+private fun <X> dfs(
+    vertex: X,
+    graph: Map<X, Collection<X>>,
+    seen: MutableSet<X>,
+    block: (x: X) -> Unit
+) {
+    if (!seen.add(vertex)) {
+        return
+    }
+    val adjacent = graph[vertex]
+    adjacent?.forEach { u: X -> dfs(u, graph, seen, block) }
+    block.invoke(vertex)
+}
+
+/**
+ * @param [graph] directed graph in form of [Map] of [Collection] of [X]
+ * @return [X] or `null` - first root
+ */
+private fun <X> firstRootOrNull(graph: Map<X, Collection<X>>): X? {
+    for (key in graph.keys) {
+        if (graph.values.none { v: Collection<X> -> v.contains(key) }) {
+            return key
+        }
+    }
+    return null
+}
+
+/**
+ * [Map] of [Collection] -> [Set]
+ */
+private fun <X> toFlatSet(graph: Map<X, Collection<X>>): Set<X> {
+    val res: MutableSet<X> = HashSet()
+    graph.forEach { (k: X, values: Collection<X>) ->
+        res.add(k)
+        res.addAll(values)
+    }
+    return res
+}
---
+++ src/test/kotlin/com/github/sszuev/ontconverter/utils/DependenciesTest.kt
@@ -0,0 +1,60 @@
+package com.github.sszuev.ontconverter.utils
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class DependenciesTest {
+
+    @Test
+    fun `test find independent components`() {
+        val graph1 = mapOf(
+            "a" to listOf(),
+            "b" to listOf("a", "c"),
+            "c" to listOf("d", "e", "f"),
+            "g" to listOf(),
+            "x" to listOf(),
+            "h" to listOf("y")
+        )
+
+        val actual1 = findIndependentComponents(graph1)
+        val expected1 = listOf(setOf("a", "d", "e", "f", "c", "b"), setOf("g"), setOf("x"), setOf("y", "h"))
+        Assertions.assertEquals(expected1, actual1)
+
+        val graph2 = mapOf(
+            "G" to listOf("F", "C", "H"),
+            "F" to listOf("G"),
+            "C" to listOf("D"),
+            "H" to listOf("D"),
+            "D" to listOf("C", "H"),
+            "A" to listOf("B"),
+            "B" to listOf("C", "F", "E"),
+            "E" to listOf("A", "F"),
+        )
+        val actual2 = findIndependentComponents(graph2)
+        val expected2 = listOf(setOf("A", "B", "C", "D", "E", "F", "G", "H"))
+        Assertions.assertEquals(expected2, actual2)
+
+        val graph3 = mapOf(
+            "f" to listOf<String>(),
+            "c" to listOf(),
+            "j" to listOf(),
+            "q" to listOf(),
+            "x" to listOf(),
+            "v" to listOf(),
+            "w" to listOf(),
+        )
+        val actual3 = findIndependentComponents(graph3)
+        val expected3 = listOf(setOf("f"), setOf("c"), setOf("j"), setOf("q"), setOf("x"), setOf("v"), setOf("w"))
+        Assertions.assertEquals(expected3, actual3)
+
+        val graph4 = mapOf(
+            "x" to listOf("y"),
+            "a" to listOf("b"),
+            "b" to listOf("c"),
+            "c" to listOf("a")
+        )
+        val actual4 = findIndependentComponents(graph4)
+        val expected4 = listOf(setOf("x", "y"), setOf("a", "b", "c"))
+        Assertions.assertEquals(expected4, actual4)
+    }
+}
