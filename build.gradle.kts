1a7bf266963bdaf465183bfc48b239656f3e69c6
sszuev <sss.zuev@gmail.com>
2022-04-23 20:26:24 +0300

    ont-converter: add javadocJar (dokka), sourceJar, publishing to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    application
+    id("org.jetbrains.dokka") version "1.6.20"
+    id("org.gradle.maven-publish")
+    id("org.gradle.application")
 }
 
 group = "com.github.sszuev"
@@ -42,27 +44,51 @@ application {
     mainClass.set("com.github.sszuev.ontconverter.MainKt")
 }
 
-tasks {
-    val fatJar = register<Jar>("fatJar") {
-        dependsOn.addAll(
-            listOf(
-                "compileJava",
-                "compileKotlin",
-                "processResources"
-            )
-        ) // We need this for Gradle optimization to work
-        archiveFileName.set(rootProject.name + ".jar")
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest {
-            attributes(mapOf("Main-Class" to application.mainClass))
-        } // Provided we set it up in the application plugin configuration
-        val sourcesMain = sourceSets.main.get()
-        val contents = configurations.runtimeClasspath.get()
-            .map { if (it.isDirectory) it else zipTree(it) } +
-                sourcesMain.output
-        from(contents)
+val fatJar = tasks.register<Jar>("fatJar") {
+    dependsOn.addAll(
+        listOf(
+            "compileJava",
+            "compileKotlin",
+            "processResources",
+            "test"
+        )
+    )
+    archiveFileName.set(rootProject.name + ".jar")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes(mapOf("Main-Class" to application.mainClass))
     }
+    val sourcesMain = sourceSets.main.get()
+    val contents = configurations.runtimeClasspath.get()
+        .map { if (it.isDirectory) it else zipTree(it) } +
+            sourcesMain.output
+    from(contents)
+}
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+    dependsOn.add(fatJar)
+}
+val dokkaHtml by tasks.getting(org.jetbrains.dokka.gradle.DokkaTask::class)
+val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+    dependsOn(dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+    dependsOn.add(sourcesJar)
+}
+
+tasks {
     build {
-        dependsOn(fatJar) // Trigger fat jar creation during build
+        dependsOn(fatJar)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+        }
     }
 }
